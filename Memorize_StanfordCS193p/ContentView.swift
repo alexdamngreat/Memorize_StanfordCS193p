dbe42cf22de1dd58a625f81f1599b7618bd34f63
timofeev <alexander.timofeev.v@gmail.com>
2021-07-20 00:36:22 +0300

    Done Programming Assignment 1.

--- Memorize_StanfordCS193p/ContentView.swift
+++ Memorize_StanfordCS193p/ContentView.swift
@@ -8,34 +8,100 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  var vehicleEmojis = ["🚗", "🚙", "🚌", "🚒", "🚐", "🚚", "🚜", "🛴", "🚂", "🚆", "🚁", "🛩"]
+  var animalEmojis = ["🐶","🐗","🐒","🐙","🐢","🐬","🐼","🦁","🦇","🐺","🦩", "🐳", "🦨", "🦦", "🦥", "🐁"]
+  var plantEmojis = ["🌳", "🌼", "🌸", "🥀", "🌹", "🍁", "🌵", "💐", "🌻", "🌿", "🌺", "🎍", "🌾", "🎄"]
+  
+  
+  @State var emojis = [""]
+  
+  
   var body: some View {
-    HStack {
-      ForEach(0..<4) { index in
-       CardView(isFaceUp: true)
+    VStack {
+      Text("Memorise!")
+        .font(.largeTitle)
+      ScrollView {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 90))]) {
+          ForEach(emojis[0..<emojis.count], id: \.self) { emoji in
+            CardView(content: emoji)
+              .aspectRatio(2/3, contentMode: .fit)
+          }
+        }
       }
+      .font(.largeTitle)
+      .foregroundColor(.red)
+      Spacer(minLength: 20)
+      HStack {
+        VStack {
+          Text("Vehicle")
+          vehicleThemeButton
+            .font(.largeTitle)
+        }
+        Spacer()
+        VStack {
+          Text("Animals")
+          animalsThemeButton
+            .font(.largeTitle)
+        }
+        Spacer()
+        VStack {
+          Text("Plants")
+          plantsThemeButton
+            .font(.largeTitle)
+        }
+      }
+      .padding(.horizontal)
     }
     .padding()
-    .foregroundColor(.orange)
-    .font(.largeTitle)
-    
-    
+  }
+  
+  var vehicleThemeButton: some View {
+    Button {
+      emojis.removeAll()
+      emojis = vehicleEmojis.shuffled()
+    } label: {
+      Image(systemName: "car.fill")
+    }
+  }
+  
+  var animalsThemeButton: some View {
+    Button {
+      emojis.removeAll()
+      emojis = animalEmojis.shuffled()
+    } label: {
+      Image(systemName: "tortoise.fill")
+    }
+  }
+  
+  var plantsThemeButton: some View {
+    Button {
+      emojis.removeAll()
+      emojis = plantEmojis.shuffled()
+    } label: {
+      Image(systemName: "leaf.fill")
+    }
   }
 }
 
+
 struct CardView: View {
-  var isFaceUp: Bool
+  var content: String
+  @State var isFaceUp: Bool = true
   
   var body: some View {
     ZStack {
+      let shape = RoundedRectangle(cornerRadius: 10.0)
       if isFaceUp {
-        RoundedRectangle(cornerRadius: 10.0)
-          .fill(Color.white)
-        RoundedRectangle(cornerRadius: 10.0)
-          .stroke(lineWidth: 3)
-        Text("👻!")
+        shape.fill(Color.white)
+        shape.strokeBorder(lineWidth: 5)
+        Text(content).font(.largeTitle)
       } else {
-        RoundedRectangle(cornerRadius: 10.0).fill()
+        shape.fill()
       }
+    }
+    .onTapGesture {
+      isFaceUp = !isFaceUp
     }
   }
 }
@@ -46,5 +112,6 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+      .preferredColorScheme(.dark)
   }
 }
